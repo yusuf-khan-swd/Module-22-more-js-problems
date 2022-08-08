312152785de38ac678532d47c00ebb7dbfcab079
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-08-08 17:21:55 +0600

    add quiz

---
+++ quiz.js
@@ -0,0 +1,16 @@
+
+// const nayoks = ["Bangla Bhai", "English Bhai", "Korean Bhai,", "Turkish Bhai"];
+
+// for (const nayok of nayoks) {
+//   console.log(nayok);
+// }
+
+// for (let i = 0; i <= 5; i++) {
+//   console.log(nayoks[i]);
+// }
+
+let sum = 0;
+for (let i = 0; i <= 3; i++) {
+  sum = sum + i;
+}
+console.log(sum);
